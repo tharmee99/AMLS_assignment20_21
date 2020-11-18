8a0ae2d37b583d586fc399b859f10c82f878721f
Tharmetharan Balendran <tharmee.99@gmail.com>
2020-11-18 18:54:45 +0000

    save/load intermediate arrays

--- AMLS_20-21_SN17011729/A1/a1_test_train.py
+++ AMLS_20-21_SN17011729/A1/a1_test_train.py
@@ -1,5 +1,7 @@
 import defs
-
+import cv2
+import os
+import numpy as np
 
 class A1(defs.Task):
     def __init__(self, dataset_dir, temp_dir):
@@ -9,7 +11,8 @@ class A1(defs.Task):
                          label_feature='gender')
 
     def preprocess_image(self, image_dir):
-        pass
+        arr = cv2.imread(image_dir, 0)
+        return arr
 
     def train(self, x_train, y_train):
         pass
@@ -19,4 +22,13 @@ class A1(defs.Task):
 
 
 if __name__ == '__main__':
-    print("asd")
+
+    root_dir = os.path.join(os.getcwd(), os.pardir)
+    A1 = A1(os.path.join(root_dir, "Datasets", "celeba"), os.path.join(os.getcwd(), "temp"))
+
+    # X, y = A1.build_design_matrix()
+
+    a = np.zeros((1, 1))
+    # A1.save_intermediate(a, "random_arr", overwrite=False)
+    A1.read_intermediate('random_arr.npy', get_latest=False)
+    pass
--- AMLS_20-21_SN17011729/defs.py
+++ AMLS_20-21_SN17011729/defs.py
@@ -26,7 +26,7 @@ class Task:
                 file_ext = idx
                 break
 
-        # If iamge cannot be found or is of incorrect format, throw exception
+        # If image cannot be found or is of incorrect format, throw exception
         if file_ext == -1:
             raise FileNotFoundError("Dataset directory was not found. Ensure that the directory is correct and images "
                                     "are of supported format")
@@ -53,6 +53,40 @@ class Task:
             raise Exception("label feature cannot be found in csv file")
 
         return X, y
+
+    def save_intermediate(self, obj, file_name, overwrite=True):
+
+        if not os.path.exists(self.temp_dir):
+            os.makedirs(self.temp_dir)
+
+        if type(obj).__module__ == np.__name__:
+            if overwrite or (not os.path.isfile(os.path.join(self.temp_dir, file_name + ".npy"))):
+                np.save(os.path.join(self.temp_dir, file_name), obj)
+            else:
+                idx = 1
+                file_name_new = file_name + '_' + str(idx)
+                while os.path.isfile(os.path.join(self.temp_dir, file_name_new + ".npy")):
+                    idx += 1
+                    file_name_new = file_name + '_' + str(idx)
+                np.save(os.path.join(self.temp_dir, file_name_new), obj)
+        else:
+            raise TypeError(type(obj) + " is not supported")
+
+    def read_intermediate(self, file_name, get_latest=False):
+        file = file_name.split('.')
+
+        if file[1] == 'npy':
+            if not get_latest:
+                return np.load(os.path.join(self.temp_dir, file_name))
+            else:
+                idx = 1
+                file_name_tmp = file[0] + '_' + str(idx)
+                while os.path.isfile(os.path.join(self.temp_dir, file_name_tmp + ".npy")):
+                    idx += 1
+                    file_name_tmp = file[0] + '_' + str(idx)
+                return np.load(os.path.join(self.temp_dir, file[0] + '_' + str(idx-1) + ".npy"))
+        else:
+            raise TypeError(file[1] + " files are not supported")
 
     def train(self, x_train, y_train):
         raise NotImplemented("Training method must be implemented!")
